afdc029afd605fcb3eacd81eb6e71174f7c9b327
Brian Porter <beporter@users.sourceforge.net>
2015-03-18 10:39:50 -0500

    Strip down UserFixture.
    Closes #9.

--- Test/Fixture/UserFixture.php
+++ Test/Fixture/UserFixture.php
@@ -13,18 +13,12 @@ class UserFixture extends CakeTestFixture {
 	public $fields = array(
 		'id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'token' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 100, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'last_login_at' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'username' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'password' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'first_name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 180, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'last_name' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 180, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'shift' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'is_operator' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
 		'is_tech' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
-		'creator_id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 36, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
-		'modifier_id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 36, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
-		'last_login_at' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1)
 		),
@@ -41,49 +35,31 @@ class UserFixture extends CakeTestFixture {
 			'id' => '7d5b22bd-fc92-11e3-b153-080027dec79b',
 			'token' => '40e9a32090f03eed13a6080e0b21a58d',
 			'username' => 'test',
+			'last_login_at' => null,
 			'password' => '49342febad93ceee33ef5635e750aacd163b2106', // == `test`
 			'first_name' => 'Test',
 			'last_name' => 'User',
-			'shift' => null,
-			'is_operator' => 0,
 			'is_tech' => 0,
-			'creator_id' => '7d5b22bd-fc92-11e3-b153-080027dec79b',
-			'created' => '2014-06-24 17:14:10',
-			'modifier_id' => '7d5b22bd-fc92-11e3-b153-080027dec79b',
-			'modified' => '2014-06-24 17:14:28',
-			'last_login_at' => null,
 		),
 		array(
 			'id' => 'c64ddc9c-7193-11e4-b74c-000c290352bb',
 			'token' => 'bb0a91cfa4f3b703531c1dc4f5f64b89',
+			'last_login_at' => '2011-06-04 13:49:06', // Will be udpated to NOW() when fixure is instantiated.
 			'username' => 'blowfish',
 			'password' => '$2a$10$GETe7PdF2xekpYqdVfK5EOGi9MsBw6MbNI.NLZkLQoDipykAy3h76',
 			'first_name' => 'Blowfish',
 			'last_name' => 'Hash',
-			'shift' => null,
-			'is_operator' => 0,
 			'is_tech' => 0,
-			'creator_id' => 1,
-			'created' => '2014-06-04 13:49:06',
-			'modifier_id' => 1,
-			'modified' => '2014-06-04 13:49:06',
-			'last_login_at' => '2011-06-04 13:49:06', // Will be udpated to NOW() when fixure is instantiated.
 		),
 		array(
 			'id' => 'aaabbbcc-7193-11e4-b74c-000c290352bb',
 			'token' => 'cc0a91cfa4f3b703531c1dc4f5f64b89',
+			'last_login_at' => '2011-06-04 13:49:06', // Will be udpated to NOW() when fixure is instantiated.
 			'username' => 'fulladmin',
 			'password' => '$2a$10$GETe7PdF2xekpYqdVfK5EOGi9MsBw6MbNI.NLZkLQoDipykAy3h76',
 			'first_name' => 'Full Admin',
 			'last_name' => 'Access',
-			'shift' => null,
-			'is_operator' => 0,
 			'is_tech' => 0,
-			'creator_id' => 1,
-			'created' => '2014-06-04 13:49:06',
-			'modifier_id' => 1,
-			'modified' => '2014-06-04 13:49:06',
-			'last_login_at' => '2011-06-04 13:49:06', // Will be udpated to NOW() when fixure is instantiated.
 		),
 	);
 
